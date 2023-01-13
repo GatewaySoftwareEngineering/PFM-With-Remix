28a3203f8e11f683b488575258c73bab8fafd62d
Hedi M. Sharif <60695654+HediMuhamad@users.noreply.github.com>
2023-01-13 16:11:10 +0300

    configuring prisma

---
+++ app/mocks/categories.js
@@ -0,0 +1,40 @@
+const categories = [
+  {
+    name: "SALARY",
+    type: "INCOME",
+  },
+  {
+    name: "LOAN",
+    type: "INCOME",
+  },
+  {
+    name: "GIFT",
+    type: "INCOME",
+  },
+  {
+    name: "TECH",
+    type: "EXPENSE",
+  },
+  {
+    name: "FOOD",
+    type: "EXPENSE",
+  },
+  {
+    name: "BILLS",
+    type: "EXPENSE",
+  },
+  {
+    name: "SPORTS",
+    type: "EXPENSE",
+  },
+  {
+    name: "HEALTH",
+    type: "EXPENSE",
+  },
+  {
+    name: "CLOTHS",
+    type: "EXPENSE",
+  },
+]
+
+export default categories
