a4f836ea55bb5c68b0f537405ac193ba392d3aba
rebaz36 <rebaz415@gmail.com>
2023-01-16 23:26:19 +0300

    update overview page

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,8 +1,59 @@
-export default function Index() {
+import Card from "~/Components/Card"
+import Transaction from "~/Components/Transaction"
+import overviewStyles from "~/styles/overview.css"
+
+export const links = () => [
+  {
+    rel: "stylesheet",
+    href: overviewStyles,
+  },
+]
+
+export default function Overview() {
+  function handleClick() {
+    console.log("Clicked")
+  }
   return (
-    <div className="index-page">
-      <h1>Finance Manager</h1>
-      <p>Let&apos;s get this done!</p>
-    </div>
-  );
+    <>
+      <div className="Main_Content__Header">
+        <h1 className="Main_Content__Text">Overview</h1>
+      </div>
+      <div className="Overview">
+        <div className="Card_Container">
+          <Card
+            value="1000000"
+            title="Income"
+            color="blue"
+            onClick={handleClick}
+          />
+          <Card
+            value="10000"
+            title="Balance"
+            color="grey"
+            onClick={handleClick}
+          />
+          <Card
+            value="100000"
+            title="Expense"
+            color="red"
+            onClick={handleClick}
+          />
+        </div>
+        <div className="week_list">
+          <h1 className="week_list_header">This Week</h1>
+          <div className="week_lists">
+            <Transaction
+              category="Edu"
+              amount="1000"
+              date="Today"
+              note="This is a note"
+            />
+          </div>
+        </div>
+        <div className="button-list">
+          <button>Add Transaction</button>
+        </div>
+      </div>
+    </>
+  )
 }
