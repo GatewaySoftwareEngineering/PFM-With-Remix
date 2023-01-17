5f61707072887c0b0ebf3b244dd8b1dd21816b3b
rebaz36 <rebaz415@gmail.com>
2023-01-17 09:51:57 +0300

    Add Transaction Component

--- app/Components/Transaction.jsx
+++ app/Components/Transaction.jsx
@@ -1,15 +1,35 @@
 import PropTypes from "prop-types"
+import { HiOutlineAcademicCap, HiOutlineChip } from "react-icons/hi"
+import { TbBriefcase } from "react-icons/tb"
 
 export default function Transaction({ category, amount, date, note }) {
+  const objDate = new Date()
+  const recivedDate = new Date(date).toLocaleDateString("en-US")
+  const today = new Date().toLocaleDateString("en-US")
+  let yestrday = new Date(objDate.getTime())
+  yestrday.setDate(objDate.getDate() - 1)
+  yestrday = yestrday.toLocaleDateString("en-US")
+
+  if (recivedDate === today) {
+    date = "Today"
+  } else if (recivedDate == yestrday) {
+    date = "Yesterday"
+  }
+  console.log(recivedDate, today, yestrday)
+  const icon = {
+    Education: <HiOutlineAcademicCap className="Blue-Icon" />,
+    Salary: <TbBriefcase className="Green-Icon" />,
+    Tech: <HiOutlineChip className="Green-Icon" />,
+  }
   return (
     <div className="transaction-item">
       <div className="transaction_head">
-        <span className="transaction_category">{category}</span>
-        <span className="transaction_date">{date}</span>
+        {icon[category]}
+        <span className="transaction_note">{note}</span>
       </div>
       <div className="transaction_body">
+        <span className="transaction_date">{date}</span>
         <span className="transaction_amount">${amount}</span>
-        <span className="transaction_note">{note}</span>
       </div>
     </div>
   )
