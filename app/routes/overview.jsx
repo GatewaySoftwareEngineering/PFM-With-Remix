48ede1535d707cf86ef52220d06eccc28b1a8aa1
Aland Abdullah <32212842+alandio@users.noreply.github.com>
2023-01-27 20:41:11 +0300

    feat: add overview page skeleton

--- app/routes/overview.jsx
+++ app/routes/overview.jsx
@@ -2,6 +2,7 @@ import styleUrl from "~/styles/overview.css"
 import cardStyleUrl from "~/styles/card.css"
 import ModalStyleUrl from "~/styles/modal.css"
 import transactionStyleUrl from "~/styles/transaction.css"
+import { useNavigate, Outlet } from "@remix-run/react"
 import Card from "~/components/card"
 import Transaction from "~/components/transaction"
 export let links = () => [
@@ -12,11 +13,28 @@ export let links = () => [
 ]
 export let meta = () => ({ title: "Overview - Personal Finance Manager" })
 export default function OverviewRoute() {
+  const navigate = useNavigate()
+
   return (
     <div className="overview-container">
       <Outlet></Outlet>
+      <div className="dashboard">
         <Card name="balance" amount={balance} />
+      </div>
+      <div className="transactions-container">
+        <div className="recent-transactions">
             <Transaction key={transaction.id} transaction={transaction} />
+        </div>
+      </div>
+      <div className="add-transaction-container">
+        <button
+          type="button"
+          className="add-transaction-button"
+          onClick={() => navigate("add")}
+        >
+          Add Transaction
+        </button>
+      </div>
     </div>
   )
 }
