d91ccad6f781f83be7029af75de36aeebb12c3b3
Aland Abdullah <32212842+alandio@users.noreply.github.com>
2023-01-27 20:28:09 +0300

    feat: implement transaction search and filters

--- app/routes/transactions.jsx
+++ app/routes/transactions.jsx
@@ -1,4 +1,8 @@
+import { useState } from "react"
 import styleUrl from "../styles/transactions.css"
+import transactionStyleUrl from "~/styles/transaction.css"
+import Transaction from "~/components/transaction"
+import { CATEGORIES as categories } from "~/utils/constants"
 export let links = () => [
   { rel: "stylesheet", href: styleUrl },
 ]
@@ -10,8 +14,59 @@ export const meta = () => {
   }
 }
 export default function Transactions() {
+  const [fromDate, setFromDate] = useState("")
+  const [toDate, setToDate] = useState("")
   return (
     <div className="transcation-history">
+      <Form method="get" action="/transactions" className="filter-bar">
+        <div className="filter-inputs">
+          <div className="filter-icon">
+            <img src="assets/images/icons/filter.svg" alt="filter" />
+          </div>
+          <input
+            type="text"
+            name="search"
+            id="search"
+            placeholder="Search"
+            autoComplete="off"
+          />
+
+          <select
+            name="category"
+            id="category"
+          >
+            <option value="">All</option>
+            {categories.map((category) => (
+              <option key={category} value={category}>
+                {category.charAt(0).toUpperCase() + category.slice(1)}
+              </option>
+            ))}
+          </select>
+          <input
+            type="date"
+            name="from-date"
+            id="from-date"
+            max={toDate || new Date().toISOString().split("T")[0]}
+            onChange={(e) => setFromDate(e.target.value)}
+          />
+          <input
+            type="date"
+            name="to-date"
+            id="to-date"
+            min={fromDate}
+            max={new Date().toISOString().split("T")[0]}
+            onChange={(e) => setToDate(e.target.value)}
+          />
+        </div>
+        <button type="submit" className="search-button">
+          Search
+        </button>
+          <button type="button" className="clear-button" onClick={clearFilters}>
+            Clear
+          </button>
+      </Form>
+      <div className="transaction-list">
+      </div>
     </div>
   )
 }
