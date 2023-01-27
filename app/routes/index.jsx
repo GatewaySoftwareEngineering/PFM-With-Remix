2a8f593fea791ffeaa84063c08b10a27789679c4
Vazhin Tayeb <vazhintayeb@gmail.com>
2023-01-27 15:10:11 +0300

    Preloads svg icon in index route & adds a sidebar

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,15 +1,31 @@
 import React from 'react'
 import { seedTransactions } from '../seeders/transaction.seeder'
 
+export const links = () => [
+  {
+    rel: 'preload',
+    href: '/assets/icons/dollar-sign.svg',
+    as: 'image',
+    type: 'image/svg+xml',
+  },
+]
+
 export default function Index() {
   React.useEffect(() => {
     seedTransactions()
   }, [])
 
   return (
-    <div className="index-page">
-      <h1>Finance Manager</h1>
-      <p>Let&apos;s get this done!</p>
+    <div className="page">
+      <div className="sidebar">
+        <div className="logo">
+          <img src="/assets/icons/dollar-sign.svg" alt="dollar-sign" />
+          <h4>Finance Manager</h4>
+        </div>
+      </div>
+      <div>
+        <p>Let&apos;s get this done!</p>
+      </div>
     </div>
   )
 }
