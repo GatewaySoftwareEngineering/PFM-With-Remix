47c1c946287fd5deb773d68d58f201f5d4ccea61
Vazhin Tayeb <vazhintayeb@gmail.com>
2023-01-28 02:22:07 +0300

    Adds overview cards to index page

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,3 +1,38 @@
+const overviewCards = [
+  {
+    title: 'Income',
+    value: '1000',
+    variant: 'info',
+  },
+  {
+    title: 'Balance',
+    value: '25000',
+    variant: 'neutral',
+  },
+  {
+    title: 'Expense',
+    value: '4520',
+    variant: 'error',
+  },
+]
+
 export default function Index() {
-  return 'overview'
+  return (
+    <div className="main-section-container">
+      <div className="overview-cards-container">
+        {overviewCards.map((card, i) => (
+          <div
+            key={i}
+            className={`overview-card ${card.variant} ${card.variant}-gradient`}
+          >
+            <div>
+              <h5>{card.title}</h5>
+              <h1 className="dollar">{card.value}</h1>
+            </div>
+            <button className={card.variant}>Details</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
