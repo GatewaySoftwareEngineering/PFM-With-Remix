597806f8ced1ef40ff5a8f8f614f3b15d34b17c5
Vazhin Tayeb <vazhintayeb@gmail.com>
2023-01-28 12:33:59 +0300

    Displays errors on form inputs from actionData

--- app/components/AddTransactionModal/index.jsx
+++ app/components/AddTransactionModal/index.jsx
@@ -1,17 +1,17 @@
 import propTypes from 'prop-types'
-import { Form, useActionData } from '@remix-run/react'
-import { categories, createTransaction } from '~/models/transaction'
+import { Form, useActionData, useTransition } from '@remix-run/react'
+import { categories } from '~/models/transaction'
 import React from 'react'
 
 export default function AddTransactionModal({ isOpen, onClose }) {
   const data = useActionData()
+  const transition = useTransition()
 
   const [type, setType] = React.useState('income')
 
-  React.useEffect(() => {
-    console.log('data', data)
-    if (data) createTransaction(data)
-  }, [data])
+  console.log('data?.errors', data?.errors)
+
+  console.log('transition', transition)
 
   return (
     <div
@@ -39,13 +39,21 @@ export default function AddTransactionModal({ isOpen, onClose }) {
               <div className="input">
                 <label htmlFor="category-input">Category</label>
 
-                <select name="category" id="category-input">
+                <select
+                  name="category"
+                  id="category-input"
+                  className={data?.errors?.category ? 'error-input' : ''}
+                >
                   {categories[type].map((category, i) => (
                     <option key={i} value={category}>
                       {category}
                     </option>
                   ))}
                 </select>
+
+                {data?.errors?.category ? (
+                  <em className="error-message">{data?.errors.category}</em>
+                ) : null}
               </div>
 
               <div className="input">
@@ -57,7 +65,12 @@ export default function AddTransactionModal({ isOpen, onClose }) {
                   name="date"
                   defaultValue={new Date().toLocaleDateString('en-ca')}
                   max={new Date().toLocaleDateString('en-ca')}
+                  className={data?.errors?.date ? 'error-input' : ''}
                 />
+
+                {data?.errors?.date ? (
+                  <em className="error-message">{data?.errors.date}</em>
+                ) : null}
               </div>
 
               <div className="input">
@@ -65,7 +78,17 @@ export default function AddTransactionModal({ isOpen, onClose }) {
 
                 <span className="amount-currency">$</span>
 
-                <input type="number" id="amount-input" name="amount" min="0" />
+                <input
+                  type="number"
+                  id="amount-input"
+                  name="amount"
+                  min="0"
+                  className={data?.errors?.amount ? 'error-input' : ''}
+                />
+
+                {data?.errors?.amount ? (
+                  <em className="error-message">{data?.errors.amount}</em>
+                ) : null}
               </div>
 
               <div className="input ">
@@ -95,6 +118,10 @@ export default function AddTransactionModal({ isOpen, onClose }) {
                     <label htmlFor="expense-input">Expense</label>
                   </div>
                 </div>
+
+                {data?.errors?.type ? (
+                  <em className="error-message">{data?.errors.type}</em>
+                ) : null}
               </div>
 
               <div className="input">
@@ -105,15 +132,25 @@ export default function AddTransactionModal({ isOpen, onClose }) {
                   name="note"
                   rows="5"
                   cols="33"
+                  maxLength={350}
+                  className={data?.errors?.amount ? 'error-input' : ''}
                 ></textarea>
+
+                {data?.errors?.note ? (
+                  <em className="error-message">{data?.errors.note}</em>
+                ) : null}
               </div>
             </div>
 
             <div className="modal-actions">
-              <button className="dismiss-button" onClick={onClose}>
+              <button type="reset" className="dismiss-button" onClick={onClose}>
                 Dismiss
               </button>
-              <button type="submit">Add Transaction</button>
+              <button type="submit">
+                {transition.state === 'submitting'
+                  ? 'Adding...'
+                  : 'Add Transaction'}
+              </button>
             </div>
           </Form>
         </div>
