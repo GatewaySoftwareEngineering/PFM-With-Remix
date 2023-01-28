158595e7554ebb5aba98687e18cdd6eaf9a3b1c9
whiteWolfx99 <hhqu180099@uniq.edu.iq>
2023-01-28 16:31:20 +0300

    Add transaction list in overview page

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,7 +1,12 @@
+import { useLoaderData } from "@remix-run/react"
 import Card from "~/components/Card"
 import CardDeatails from "~/data/CardDeatails"
-import { useLoaderData } from "@remix-run/react"
-import { getLastMonthTransactions } from "~/utils/transactions"
+import {
+  getLastWeekTransactions,
+  getLastMonthTransactions,
+  getLastYearTransactions,
+} from "~/utils/transactions"
+import TransactionItem from "~/components/TransactionItem"
 
 export const loader = async () => {
   const res = await fetch(" http://localhost:8000/mockedTransactions", {
@@ -17,6 +22,27 @@ export default function Index() {
 
   // for card month balance
   const monthAchievements = getLastMonthTransactions(transactions)
+  // for transaction
+  const thisWeekTransactions = getLastWeekTransactions(transactions)
+  const thisMonthTransactions = getLastMonthTransactions(transactions)
+  const thisYearTransactions = getLastYearTransactions(transactions)
+  let showntransactions = []
+  let shownTitle = ""
+
+  if (thisWeekTransactions.length > 0) {
+    showntransactions = thisWeekTransactions
+    shownTitle = "This Week"
+  } else if (thisMonthTransactions.length > 0) {
+    showntransactions = thisMonthTransactions
+    shownTitle = "This Month"
+  } else if (thisYearTransactions.length > 0) {
+    showntransactions = thisYearTransactions
+    shownTitle = "This Year"
+  } else {
+    showntransactions = transactions
+    shownTitle = "All Transactions"
+  }
+
   return (
     <div className="overview-page">
       <div className="card_row">
@@ -28,6 +54,24 @@ export default function Index() {
             mockedTransactions={monthAchievements}
           />
         ))}
+      </div>
+      <div className="overview_transaction_container">
+        <h2>{shownTitle}</h2>
+        <div className="overview_transaction_data">
+          {showntransactions
+            .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
+            .slice(0, 10)
+            .map((transaction) => (
+              <TransactionItem
+                key={transaction.id}
+                note={transaction.note}
+                amount={transaction.amount}
+                date={transaction.createdAt}
+                category={transaction.category}
+                type={transaction.type}
+              />
+            ))}
+        </div>
       </div>
     </div>
   )
