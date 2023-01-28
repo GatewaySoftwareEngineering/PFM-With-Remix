60fb93e81a51001c67b03f32910a25e5ec029ce8
Vazhin Tayeb <vazhintayeb@gmail.com>
2023-01-28 04:37:01 +0300

    Adds transaction cards and list to overview page

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,26 +1,51 @@
-const overviewCards = [
+import React from 'react'
+import { displayRelativeDate } from '~/helpers/displayRelativeDate'
+import { filterByRelativeTime } from '~/helpers/filterByRelativeTime'
+import { getTransactions } from '~/models/transaction'
+
+const overviewCards = (transactions) => [
   {
     title: 'Income',
-    value: '1000',
+    value: filterByRelativeTime(transactions, 'month').reduce(
+      (acc, transaction) => {
+        if (transaction.type === 'income') return acc + transaction.amount
+        else return acc
+      },
+      0
+    ),
     variant: 'info',
   },
   {
     title: 'Balance',
-    value: '25000',
+    value: transactions.reduce((acc, transaction) => {
+      return acc + transaction.amount
+    }, 0),
     variant: 'neutral',
   },
   {
     title: 'Expense',
-    value: '4520',
+    value: filterByRelativeTime(transactions, 'month').reduce(
+      (acc, transaction) => {
+        if (transaction.type === 'expense') return acc + transaction.amount
+        else return acc
+      },
+      0
+    ),
     variant: 'error',
   },
 ]
 
 export default function Index() {
+  const [transactions, setTransactions] = React.useState([])
+
+  React.useEffect(() => {
+    setTransactions(getTransactions())
+  }, [])
+
   return (
-    <div className="main-section-container">
+    <div className="main-section">
       <div className="overview-cards-container">
-        {overviewCards.map((card, i) => (
+        {overviewCards(transactions).map((card, i) => (
           <div
             key={i}
             className={`overview-card ${card.variant} ${card.variant}-gradient`}
@@ -32,6 +57,28 @@ export default function Index() {
             <button className={card.variant}>Details</button>
           </div>
         ))}
+      </div>
+
+      <div className="transactions-section">
+        <h4>This Week</h4>
+
+        <div className="transactions-list">
+          {transactions.map((transaction, i) => (
+            <div key={i} className="transaction-card">
+              <div>
+                <div className="transaction-card-icon"></div>
+                <p className="transaction-note">{transaction.note}</p>
+              </div>
+
+              <div>
+                <p>{displayRelativeDate(transaction.date)}</p>
+                <div className="transaction-amount-chip">
+                  <p className="dollar">{transaction.amount}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   )
