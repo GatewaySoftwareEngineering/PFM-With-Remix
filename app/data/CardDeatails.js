a8ac9b27a7d508d5e62b2ddf5e4dc998a9acf0b7
whiteWolfx99 <hhqu180099@uniq.edu.iq>
2023-01-28 16:00:21 +0300

    Add CardDeatails Data

---
+++ app/data/CardDeatails.js
@@ -0,0 +1,16 @@
+const CardDeatails = [
+  {
+    className: "income",
+    title: "Income",
+  },
+  {
+    className: "balance",
+    title: "Balance",
+  },
+  {
+    className: "expense",
+    title: "Expense",
+  },
+]
+
+export default CardDeatails
