f8acca5cabca1237b2f15ada088818f7763bf1bd
whiteWolfx99 <hhqu180099@uniq.edu.iq>
2023-01-28 16:19:24 +0300

    Add Cards

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,8 +1,34 @@
+import Card from "~/components/Card"
+import CardDeatails from "~/data/CardDeatails"
+import { useLoaderData } from "@remix-run/react"
+import { getLastMonthTransactions } from "~/utils/transactions"
+
+export const loader = async () => {
+  const res = await fetch(" http://localhost:8000/mockedTransactions", {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  })
+  const data = await res.json()
+  return data
+}
+
 export default function Index() {
+  const transactions = useLoaderData()
+
+  // for card month balance
+  const monthAchievements = getLastMonthTransactions(transactions)
   return (
-    <div className="index-page">
-      <h1>Finance Manager</h1>
-      <p>Let&apos;s get this done!</p>
+    <div className="overview-page">
+      <div className="card_row">
+        {CardDeatails.map((card) => (
+          <Card
+            key={card.title}
+            className={card.className}
+            title={card.title}
+            mockedTransactions={monthAchievements}
+          />
+        ))}
+      </div>
     </div>
-  );
+  )
 }
