e0b73ee7b7884fc506ceeb5bd95ccaf6138160bd
Vazhin Tayeb <vazhintayeb@gmail.com>
2023-01-28 05:17:21 +0300

    Creates object form commonDurations

---
+++ app/helpers/commonDurations.js
@@ -0,0 +1,21 @@
+export const commonDurations = {
+  week: {
+    id: 'week',
+    label: 'Week',
+    days: 7,
+  },
+  month: {
+    id: 'month',
+    label: 'Month',
+    days: 30,
+  },
+  year: {
+    id: 'year',
+    label: 'Year',
+    days: 365,
+  },
+  all: {
+    id: 'all',
+    label: 'All',
+  },
+}
