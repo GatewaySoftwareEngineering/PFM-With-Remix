832760b94dbd362318db4b81b34129b192821c3e
whiteWolfx99 <hhqu180099@uniq.edu.iq>
2023-01-28 15:30:58 +0300

    Add icons and logo

---
+++ app/assets/Icons/Cloths.jsx
@@ -0,0 +1,29 @@
+const Cloths = () => {
+  return (
+    <svg
+      width={32}
+      height={32}
+      viewBox="0 0 32 32"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle cx={16} cy={16} r={16} fill="#C8B5FF" />
+      <path
+        d="M13 10.9583H19C21.8333 10.9583 22.1167 12.4158 22.3083 14.1942L23.0583 21.0692C23.3 23.3242 22.6667 25.1667 19.75 25.1667H12.2583C9.33332 25.1667 8.69999 23.3242 8.94999 21.0692L9.69999 14.1942C9.88332 12.4158 10.1667 10.9583 13 10.9583Z"
+        stroke="#660076"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M12.6667 12.3333V9.125C12.6667 7.75 13.5 6.83334 14.75 6.83334H17.25C18.5 6.83334 19.3333 7.75 19.3333 9.125V12.3333M23.0083 20.6108H12.6667"
+        stroke="#660076"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  )
+}
+
+export default Cloths
