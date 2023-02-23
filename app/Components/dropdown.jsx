089ca6026327834c94e05e8a278c6b2ed87e105d
rebaz36 <rebaz415@gmail.com>
2023-02-23 12:32:49 +0300

    update dropdown MultiSelection

--- app/Components/dropdown.jsx
+++ app/Components/dropdown.jsx
@@ -1,22 +1,35 @@
 import Select from "react-select"
 import PropTypes from "prop-types"
 
-export default function Dropdown({ title, options, setCategory }) {
+export default function Dropdown({ title, options, setCategory, defaultValue, isMulti }) {
   const onChange = (e) => {
+    if (isMulti) {
+      const categories = e.map((item) => item.value)
+      return setCategory(categories)
+    }
     setCategory(e.value)
   }
   return (
     <div className="elements">
       {" "}
-      <label className="input-title">{title}</label>
-      <Select name="select" options={options} onChange={onChange} required />
+      {title.length?  <label className="input-title">{title}</label> : null}
+      <Select name="select" options={options} onChange={onChange} defaultValue={defaultValue} isMulti={isMulti} required />
     </div>
   )
 }
 
+// default props
+Dropdown.defaultProps = {
+  title: "",
+  defaultValue: "",
+  isMulti: false,
+}
+
 // props validation
 Dropdown.propTypes = {
-  title: PropTypes.string.isRequired,
+  title: PropTypes.string,
+  defaultValue: PropTypes.object,
   options: PropTypes.array.isRequired,
   setCategory: PropTypes.func.isRequired,
+  isMulti: PropTypes.bool,
 }
