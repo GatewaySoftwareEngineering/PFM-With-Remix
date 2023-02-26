1c656115e8c1cd3d82f7ee94142ee7740bec0e24
rebaz36 <rebaz415@gmail.com>
2023-02-26 13:26:49 +0300

    Update Custome Select component

--- app/Components/dropdown.jsx
+++ app/Components/dropdown.jsx
@@ -1,7 +1,7 @@
 import Select from "react-select"
 import {PropTypes} from "prop-types"
 
-export default function Dropdown({ title, options, setCategory, defaultValue, isMulti }) {
+export default function Dropdown({ name, title, options, setCategory, defaultValue, isMulti }) {
   const onChange = (e) => {
     if (isMulti) {
       const categories = e.map((item) => item.value)
@@ -13,7 +13,7 @@ export default function Dropdown({ title, options, setCategory, defaultValue, is
     <div className="elements">
       {" "}
       {title.length?  <label className="input-title">{title}</label> : null}
-      <Select name="select" options={options} onChange={onChange} defaultValue={defaultValue} isMulti={isMulti} required />
+      <Select id="long-value-select" instanceId="long-value-select" htmlFor={name} name={name} options={options} onChange={onChange} defaultValue={defaultValue} isMulti={isMulti} required />
     </div>
   )
 }
@@ -23,13 +23,15 @@ Dropdown.defaultProps = {
   title: "",
   defaultValue: "",
   isMulti: false,
+  name: "",
 }
 
 // props validation
 Dropdown.propTypes = {
   title: PropTypes.string,
-  defaultValue: PropTypes.object,
+  defaultValue: PropTypes.string,
   options: PropTypes.array.isRequired,
   setCategory: PropTypes.func.isRequired,
   isMulti: PropTypes.bool,
+  name: PropTypes.string,
 }
