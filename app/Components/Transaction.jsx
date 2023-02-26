1f2a5401997683a1eed33372e5122319e3ec53cc
rebaz36 <rebaz415@gmail.com>
2023-02-26 13:28:42 +0300

    add icons for transactions

--- app/Components/Transaction.jsx
+++ app/Components/Transaction.jsx
@@ -1,6 +1,11 @@
-import {PropTypes} from "prop-types"
+import { PropTypes } from "prop-types"
 import { HiOutlineAcademicCap, HiOutlineChip } from "react-icons/hi"
+import { AiOutlineGift } from "react-icons/ai"
 import { TbBriefcase } from "react-icons/tb"
+import { IoFastFoodOutline } from "react-icons/io5"
+import { CiMoneyBill } from "react-icons/ci"
+import { MdOutlineSportsTennis, MdOutlineHealthAndSafety } from "react-icons/md"
+import { GiClothes } from "react-icons/gi"
 
 export default function Transaction({ category, amount, date, note }) {
   const objDate = new Date()
@@ -17,11 +22,34 @@ export default function Transaction({ category, amount, date, note }) {
     date = "Yesterday"
   }
 
-  const icon = {
-    Education: <HiOutlineAcademicCap className="Blue-Icon" />,
-    Salary: <TbBriefcase className="Green-Icon" />,
-    Tech: <HiOutlineChip className="Green-Icon" />,
+  const formatAmount = (amount) => {
+    if (amount >= 1000000000) {
+      return (amount / 1000000000).toFixed(1) + "B"
+    } else if (amount >= 1000000) {
+      return (amount / 1000000).toFixed(1) + "M"
+    } else if (amount >= 1000) {
+      return (amount / 1000).toFixed(1) + "K"
+    } else {
+      return amount
+    }
   }
+
+  const icon = {
+    Salary: <TbBriefcase className="Green-Icon" />,
+    Loan: <TbBriefcase className="Green-Icon" />,
+    Gift: <AiOutlineGift className="Blue-Icon" />,
+
+    Tech: <HiOutlineChip className="Green-Icon" />,
+    Food: <IoFastFoodOutline className="Blue-Icon" />,
+    Bills: <CiMoneyBill className="Green-Icon" />,
+    Sports: <MdOutlineSportsTennis className="Blue-Icon" />,
+    Health: <MdOutlineHealthAndSafety className="Blue-Icon" />,
+    Cloths: <GiClothes className="Green-Icon" />,
+    Education: <HiOutlineAcademicCap className="Blue-Icon" />,
+  }
+
+  const TypeIncomeOptions = ["Loan", "Salary", "Gift"]
+
   return (
     <div className="transaction-item">
       <div className="transaction_head">
@@ -30,7 +58,7 @@ export default function Transaction({ category, amount, date, note }) {
       </div>
       <div className="transaction_body">
         <span className="transaction_date">{date}</span>
-        <span className="transaction_amount">${amount}</span>
+        <span className={`${TypeIncomeOptions.includes(category)? "transaction_amount__income" : "transaction_amount__expense"}`}>${formatAmount(amount)}</span>
       </div>
     </div>
   )
