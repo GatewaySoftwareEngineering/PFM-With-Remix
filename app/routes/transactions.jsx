6fa3cb5bc858fb63b40823465fcca8d7a52f676d
Ayub Abdullah <ayubgardy898@gmail.com>
2023-03-04 10:12:40 +0300

    creating transactions route and component

---
+++ app/routes/transactions.jsx
@@ -0,0 +1,13 @@
+import Topbar from "~/components/Topbar"
+
+export default function Transactions() {
+  return (
+    <div>
+      <Topbar title="Transaction History" />
+      <ul>
+        <li>Transaction 1</li>
+        <li>Transaction 2</li>
+      </ul>
+    </div>
+  )
+}
