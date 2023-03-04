42d2b0ed6910b8b7504de079673d84ecb71fb34c
Ayub Abdullah <ayubgardy898@gmail.com>
2023-03-04 17:24:46 +0300

    adding prisma

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -2,6 +2,7 @@ import Cards from "~/components/Cards"
 import LatestTransactions from "~/components/LatestTransactions"
 import Topbar from "~/components/Topbar"
 
+
 export default function Index() {
   return (
     <>
