474328d0981d782d6b5fbb0b9e158bcc9acabd46
Ayub Abdullah <ayubgardy898@gmail.com>
2023-03-05 12:11:36 +0300

    creating AddTransactionModal component

---
+++ app/components/AddTransactionModal.jsx
@@ -0,0 +1,11 @@
+export default function AddTransactionModal() {
+  return (
+    <div className="modal">
+      <div className="modal-content">
+        <div className="modal-header">header</div>
+        <div className="modal-body">body</div>
+        <div className="modal-footer">footer</div>
+      </div>
+    </div>
+  )
+}
--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,11 +1,12 @@
+import AddTransactionModal from "~/components/AddTransactionModal"
 import Cards from "~/components/Cards"
 import LatestTransactions from "~/components/LatestTransactions"
 import Topbar from "~/components/Topbar"
 
-
 export default function Index() {
   return (
     <>
+      <AddTransactionModal />
       <Topbar title="Overview" />
       <Cards />
       <LatestTransactions />
